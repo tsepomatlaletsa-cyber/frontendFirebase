f9d5d5f0455984a5967205d67c5521677d9133a3
Bolotin Paduka <bolotinmatlaletsa@gmail.com>
2025-11-02 19:14:04 -1200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend
+  baseURL: "https://backendfirebase-a1f4.onrender.com", // backend
 });
 
 export default API;
